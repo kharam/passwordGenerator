87a19dba6f71c6fd82433423ca1cdcd9a2c5b72a
Haram Kwon <kharam@uw.edu>
2021-01-11 22:31:32 -0800

    generate a char helper function added

--- script.js
+++ script.js
@@ -7,10 +7,50 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 function generatePassword() {
+  function getUserconfirmation() {}
+
+  function generatePasswordWithCriteria() {
+    // return random lowercase alphabet.
+    function randomLowerChar() {
+      const alphabet = "abcdefghijklmnopqrstuvwxyz";
+      const randomChar = generateRandomFromCharSet(alphabet);
+
+      return randomChar;
+    }
+
+    // return random uppercase alphabet.
+    function randomUpperChar() {
+      return randomLowerChar().toUpperCase();
+    }
+
+    // return random number.
+    function randomNumber() {
+      return Math.floor(Math.random() * 10).toString();
+    }
+
+    // return random special character.
+    function randomSpecialChar() {
+      const charSet = "a-z A-Z, 0-9, ~!@#$%^&*()|}{}/.,<>?";
+      const randomChar = generateRandomFromCharSet(charSet);
+
+      return randomChar;
+    }
+
+    function generateRandomFromCharSet(charSet) {
+      const charArray = charSet.split("");
+      const random = Math.floor(Math.random() * charArray.length);
+
+      return charArray[random];
+    }
+  }
+  // possible password sets are a-z A-Z, 0-9, ~!@#$%^&*()|}{}/.,<>?
+
+  // Get user confirmation regarding character types
+
+  // With the criterial generating the password
 }
 
 // Add event listener to generate button
